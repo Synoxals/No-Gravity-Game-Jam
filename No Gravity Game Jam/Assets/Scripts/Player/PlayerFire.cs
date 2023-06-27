44d9f57d353c8d8675efed405232feafb11a6643
Synoxals <536662@chrome.escg.ac.uk>
2023-06-28 00:15:33 +0100

    Blaster particles

--- No Gravity Game Jam/Assets/Scripts/Player/PlayerFire.cs
+++ No Gravity Game Jam/Assets/Scripts/Player/PlayerFire.cs
@@ -14,6 +14,7 @@ public class PlayerFire : MonoBehaviour
     Animator anim;
     public float timerMax;
     public float shootTimer;
+    public ParticleSystem FireParticles;
 
     [Header("Kills Stuff")]
     public int Kills = 0;
@@ -74,6 +75,7 @@ public class PlayerFire : MonoBehaviour
                 
                 StartCoroutine(Shoot());
                 blasterSound.Play();
+                FireParticles.Play();
 
             }
         }
